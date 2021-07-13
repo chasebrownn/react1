9f6369f7590eb8d24f0549203aeba2be9c88a44e
Chase Brown <Chasebrown7@icloud.com>
2021-07-12 17:39:53 -0700

    full team updated - missing Will and Ben

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -46,8 +46,8 @@ function Cards() {
                         />
                     </div>
                     <div className="col-12 col-lg-4">
-                        <CardItem src={require('./content/images/meme1.jpg').default}
-                            text="Cory"
+                        <CardItem src={require('./content/images/cory_headshot.JPG').default}
+                            text="Cory Carruth"
                             label="Noble CEO"
                             path="/services"
                         />
@@ -55,11 +55,24 @@ function Cards() {
                     <div className="col-12 col-lg-4">
                         <CardItem src={require('./content/images/Matt-Headshot.jpg').default}
                             text="Matthew Merrill"
-                            label="Elevate Developer"
+                            label="Software Developer"
                             path="/services"
                         />
                     </div>
-
+                    <div className="col-12 col-lg-4">
+                        <CardItem src={require('./content/images/chase-headshot.png').default}
+                            text="Chase Brown"
+                            label="Software Developer"
+                            path="/services"
+                        />
+                    </div>
+                    <div className="col-12 col-lg-4">
+                        <CardItem src={require('./content/images/zack_headshot.jpg').default}
+                            text="Zack Swore"
+                            label="Web Developer"
+                            path="/services"
+                        />
+                    </div>
 
                 </div>
             </div>
