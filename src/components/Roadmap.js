b5c9a28b48f237c6da4cd8635a6b7cbd54bfc2c0
Chase Brown <Chasebrown7@icloud.com>
2021-07-15 16:48:43 -0700

    roadmap verbage changed

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -21,9 +21,9 @@ function Roadmap() {
                                         <ul class="timeline">
                                             <li class="event" data-date="Quarter 1">
                                                 <h3>Launch and List</h3>
-                                                <p>The most fun part of our roadmap and that is launching our new cryptocurrency.
-                                                Our goal is to launch, establish our digital footprint, including social media and web
-                                                presence, and get listed on exchanges such as CoinMarketCap and CoinGecko. Our
+                                                <p>The most fun part of our roadmap... launching our new cryptocurrency.
+                                                Our goal is to launch, establish our digital footprint on social media, and get
+                                                listed on exchanges such as CoinMarketCap and CoinGecko. Our
                                                 initial goal is to spread our purpose amongst the esports space and create momentum
                                                 before launching tournaments and taking advantage of our prize pool.</p>
                                             </li>
@@ -36,16 +36,17 @@ function Roadmap() {
                                             </li>
                                             <li class="event" data-date="Quarter 3">
                                                 <h3>Scale</h3>
-                                                <p>Scale Nobility team and competitions. Maintain token utility and establish more exchanges.
-                                                Establish Nobility as a payment option on other platforms, such as Twitch, Blizzard, Steam,
-                                                and organizations alike. In Q2 we also intend to integrate Noble players and contracts onto
+                                                <p>In quarter 3 our goal is to scale Nobility team and competitions. Maintain token utility
+                                                and establish more exchanges. We also plan on establishing Nobility as a
+                                                payment option on other platforms, such as Twitch, Blizzard, Steam,
+                                                and organizations alike. In Q3 we also intend to integrate Noble players and contracts onto
                                                 Nobility’s blockchain for easier and more efficient salary payouts and competition bonuses.</p>
                                             </li>
                                             <li class="event" data-date="Quarter 4">
                                                 <h3>Elevate</h3>
                                                 <p>As the Nobility token continues to elevate not only as a currency, but as an esports based
-                                                brand, in Q4 we plan on introducing annual Nobility LAN tournaments that will spotlight
-                                                the biggest esports content creators and influencers for a significant price pool. We plan on
+                                                brand, we plan on introducing annual Nobility LAN tournaments that will spotlight
+                                                the biggest esports content creators and influencers for a significant prize pool. We plan on
                                                 scaling our blockchain contract system as we adopt more gamers and gaming teams alike.
                                                 We also intend on developing a skin and in-game NFT marketplace and create our own
                                                 sportsbook allowing people to safely bet on esports tournaments and teams using Nobility
