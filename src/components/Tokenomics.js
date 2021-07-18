0ec207c64915a850c8d841aa5daffedf954c45ea
HankWyatt <lwyatt.henry@gmail.com>
2021-07-18 16:24:19 -0400

    chore: modify tokenomics text.

--- src/components/Tokenomics.js
+++ src/components/Tokenomics.js
@@ -14,7 +14,8 @@ function Tokenomics() {
                     </div>
 
                     <p className="mb-5 pd-5">For all network transactions done with Nobility: 4% is redistributed by weight to all existing holders on the Binance Smart Chain,
-                        4% go toward our liquidity pool, 2% will be sent to a prize pool wallet which will represent the total prize pool for our monthly esports tournaments.
+                        4% go toward our liquidity pool, 2% will be sent to the Nobility Use wallet, which will be used for, including, but not limited to, Esports tournament prize pools,
+                        marketing, business development, charity donations, scholarship funding, manual burns, community events, and giveaways.
                         The burn address is receiving reflection rewards the same as a holder would giving the token an inherent burn, which will fluctuate dependant on volume.</p>
                     <div className="tokenomics-display">
                         <img img class="img-fluid" src={require('./content/images/tokenomics-main.png').default} className="roadmap-img" />
